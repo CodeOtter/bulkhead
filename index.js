c72cd3f89272c342f6e8f279b38d39d7fc82a9c3
12dCode <12dCode@gmail.com>
2014-08-05 00:24:48 -0700

    Adding bootstrapping

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ module.exports = {
 	service: require('./lib/Service.js'),
 	response: require('./lib/ServiceResultCollection.js'), 
 	result: require('./lib/ServiceResult.js'),
-	bootsrap: require('./lib/Bootstrap.js')
+	bootstrap: require('./lib/Bootstrap.js')
 };
